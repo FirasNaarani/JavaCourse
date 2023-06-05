59ba5b24470c24a69436343998d8952e75f1ad69
Firas Naarani <firas.narani.1999@gmail.com>
2023-06-05 14:14:47 +0300

    Day 3 - Done

---
+++ src/Pack2/BankAccount.java
@@ -0,0 +1,26 @@
+package Pack2;
+
+public class BankAccount {
+    String accountNumber;
+    double balance = 0;
+
+    public BankAccount(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount > balance)
+        {
+            throw new InsufficientFundsException("Insufficient funds! Cannot withdraw more than the available balance.");
+        }
+        balance = balance - amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/Pack2/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package Pack2;
+
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
